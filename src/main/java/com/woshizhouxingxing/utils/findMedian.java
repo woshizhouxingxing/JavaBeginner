f274ffeff5e44f48c06b5cc98a46c7815c4aa561
woshizhouxingxing <444967134@qq.com>
2020-08-12 13:34:03 +0800

    add 寻找两个正序数组的中位数

---
+++ src/main/java/com/woshizhouxingxing/utils/findMedian.java
@@ -0,0 +1,20 @@
+package com.woshizhouxingxing.utils;
+
+
+import java.util.Arrays;
+
+public class findMedian {
+    /**
+     * 寻找两个正序数组的中位数
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums = Arrays.copyOf(nums1, nums1.length + nums2.length);
+        System.arraycopy(nums2, 0, nums, nums1.length, nums2.length);
+        Arrays.sort(nums);
+        return nums.length % 2 == 0 ? (nums[nums.length / 2] + nums[nums.length / 2 - 1]) / 2.0 : nums[nums.length / 2];
+    }
+}
---
+++ src/test/java/com/woshizhouxingxing/utils/findMedianSortedArrays.java
@@ -0,0 +1,9 @@
+package com.woshizhouxingxing.utils;
+
+public class findMedianSortedArrays {
+    public static void main(String[] args) {
+        int[] num1 = new int[]{1, 3};
+        int[] num2 = new int[]{2, 4};
+        System.out.println(new findMedian().findMedianSortedArrays(num1,num2));
+    }
+}
