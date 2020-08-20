7e48a5eb29ad1f888eadb07684fa194afa5a1d6b
woshizhouxingxing <444967134@qq.com>
2020-08-20 15:07:22 +0800

    update

---
+++ src/main/java/com/woshizhouxingxing/utils/ListNode.java
@@ -0,0 +1,10 @@
+package com.woshizhouxingxing.utils;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
--- src/main/java/com/woshizhouxingxing/utils/TwoSum.java
+++ src/main/java/com/woshizhouxingxing/utils/TwoSum.java
@@ -3,19 +3,20 @@ package com.woshizhouxingxing.utils;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- *
- * 示例:
- *
- * 给定 nums = [2, 7, 11, 15], target = 9
- *
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
- */
+
 public class TwoSum {
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     *
+     * 示例:
+     *
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     *
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     */
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -27,5 +28,47 @@ public class TwoSum {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+
+    /**
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     *
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     *
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * 示例：
+     *
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) {
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
 
 }
