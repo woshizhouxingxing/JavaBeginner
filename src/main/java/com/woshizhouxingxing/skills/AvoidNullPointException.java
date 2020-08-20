613a1a9faf74e233f59f0b77e67d2df6fa27e397
woshizhouxingxing <444967134@qq.com>
2020-08-20 15:36:42 +0800

    add

---
+++ src/main/java/com/woshizhouxingxing/skills/AvoidNullPointException.java
@@ -0,0 +1,23 @@
+package com.woshizhouxingxing.skills;
+
+import java.util.Objects;
+
+/**
+ * @author W12571
+ */
+public class AvoidNullPointException {
+    public static void main(String[] args) {
+        String str = null;
+
+        if ("abc".equals(str)) { // no NPE here
+            System.out.println("The same");
+        }
+
+        String s1 = null;
+        String s2 = null;
+
+        if (Objects.equals(s1, s2)) { // no NPE here
+            System.out.println("Strings are the same");
+        }
+    }
+}
